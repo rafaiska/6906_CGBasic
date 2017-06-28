65f5e7abebd0b97a0f5022a391c773ef2ca9ddc5
Rafael Cortez Sanchez <ra82357@uem.br>
2017-06-27 23:07:58 -0300

    funcionalidades prontas, corrigir bugs

--- transformation.py
+++ transformation.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from PIL import ImageTk
 
@@ -21,26 +22,119 @@ def piltomatrix(pilimage):
     for i in range(pilimage.size[0]):
         for j in range(pilimage.size[1]):
             if pixels[i, j] != 255:
-                list_x.append(j)
-                list_y.append(i)
+                list_x.append(i)
+                list_y.append(pilimage.size[1] - j)
 
-    matrix = np.matrix([list_x, list_y, [0] * len(list_x)])
+    matrix = np.matrix([list_x, list_y, [1] * len(list_x)])
     return matrix
 
 
-def move(pilimage, canvas, mvx, mvy):
+def scale(pilimage, canvas, scx, scy):
+    def findoriginpx():
+        pixels = pilimage.load()
+        orgx = 0
+        orgy = 0
+        for j in xrange(pilimage.size[1]):
+            for i in xrange(pilimage.size[0] - 1, 0, -1):
+                if pixels[i, j] != 255:
+                    orgy = j
+        for i in xrange(pilimage.size[0] - 1, 0, -1):
+            for j in xrange(pilimage.size[1]):
+                if pixels[i, j] != 255:
+                    orgx = i
+        return (orgx, pilimage.size[1] - orgy)
+
     matrix = piltomatrix(pilimage)
-    transformation = np.matrix([[1, 0, mvx],
-                               [0, 1, mvy],
-                               [0, 0, 1]])
+    fsp = findoriginpx()
+    transformation = np.matrix([[scx, 0, fsp[0] - fsp[0] * scx],
+                                [0, scy, fsp[1] - fsp[1] * scy],
+                                [0, 0, 1]])
     results = transformation * matrix
     clear(pilimage, canvas)
     pixels = pilimage.load()
 
     for column in xrange(results.shape[1]):
-        ptx = results[0, column]
-        pty = results[1, column]
-        pixels[ptx, pty] = 0
+        ptx = int(results[0, column])
+        pty = int(results[1, column])
+        try:
+            pixels[ptx, pilimage.size[1] - pty] = 0
+        except IndexError:
+            pass
 
     canvas._image_tk = ImageTk.PhotoImage(pilimage)
     canvas.itemconfigure(canvas._image_id, image=canvas._image_tk)
+
+
+def move(pilimage, canvas, mvx, mvy):
+    matrix = piltomatrix(pilimage)
+    transformation = np.matrix([[1, 0, mvx],
+                                [0, 1, mvy],
+                                [0, 0, 1]])
+    results = transformation * matrix
+    clear(pilimage, canvas)
+    pixels = pilimage.load()
+
+    for column in xrange(results.shape[1]):
+        ptx = int(results[0, column])
+        pty = int(results[1, column])
+        try:
+            pixels[ptx, pilimage.size[1] - pty] = 0
+        except IndexError:
+            pass
+
+    canvas._image_tk = ImageTk.PhotoImage(pilimage)
+    canvas.itemconfigure(canvas._image_id, image=canvas._image_tk)
+
+
+def rotate(pilimage, canvas, angle):
+    def findoriginpx():
+        pixels = pilimage.load()
+        orgx = 0
+        orgy = 0
+        for j in xrange(pilimage.size[1]):
+            for i in xrange(pilimage.size[0] - 1, 0, -1):
+                if pixels[i, j] != 255:
+                    orgy = j
+        for i in xrange(pilimage.size[0] - 1, 0, -1):
+            for j in xrange(pilimage.size[1]):
+                if pixels[i, j] != 255:
+                    orgx = i
+        return (orgx, pilimage.size[1] - orgy)
+
+    angle = - angle * math.pi / 180.0  # Converter para radianos
+    fsp = findoriginpx()
+    matrix = piltomatrix(pilimage)
+    mvx = fsp[1] * math.sin(angle) - fsp[0] * math.cos(angle) + fsp[0]
+    mvy = - fsp[0] * math.sin(angle) - fsp[1] * math.cos(angle) + fsp[1]
+    transformation = np.matrix([[math.cos(angle), - math.sin(angle), mvx],
+                                [math.sin(angle), math.cos(angle), mvy],
+                                [0, 0, 1]])
+    results = transformation * matrix
+    clear(pilimage, canvas)
+    pixels = pilimage.load()
+
+    for column in xrange(results.shape[1]):
+        ptx = int(results[0, column])
+        pty = int(results[1, column])
+        try:
+            pixels[ptx, pilimage.size[1] - pty] = 0
+        except IndexError:
+            pass
+
+    canvas._image_tk = ImageTk.PhotoImage(pilimage)
+    canvas.itemconfigure(canvas._image_id, image=canvas._image_tk)
+
+
+def zoom(pilimage, canvas, vertix1, vertix2):
+    minx = min(vertix1[0], vertix2[0])
+    miny = min(vertix1[1], vertix2[1])
+    maxx = max(vertix1[0], vertix2[0])
+    maxy = max(vertix1[1], vertix2[1])
+    width = float(maxx - minx)
+    height = float(maxy - miny)
+    if width > height:
+        zoomrate = float(pilimage.size[0]) / width
+    else:
+        zoomrate = float(pilimage.size[1]) / height
+    move(pilimage, canvas, - minx, - miny)
+    scale(pilimage, canvas, zoomrate, zoomrate)
--- workcanvas.py
+++ workcanvas.py
@@ -54,6 +54,11 @@ def _drawrect(clickstack, canvas, img):
     canvas._image_tk = ImageTk.PhotoImage(img)
     canvas.itemconfigure(canvas._image_id, image=canvas._image_tk)
 
+def _zoom(clickstack, canvas, img):
+    vx1 = clickstack.get_nowait()
+    vx2 = clickstack.get_nowait()
+    transformation.zoom(img, canvas, vx1, vx2)
+
 
 def _drawtrian(clickstack, canvas, img):
     vx1 = clickstack.get_nowait()
@@ -84,44 +89,134 @@ def _clear(img, canvas):
 
 
 def _move(img, canvas):
-    transformation.move(img, canvas, 20, 0)
+    def press(xtext, ytext):
+        top.destroy()
+        transformation.move(img, canvas, int(xtext), int(ytext))
+
+    top = Tkinter.Toplevel(width=300, height=300)
+    top.title('Translacao')
+
+    msg = Tkinter.Message(top, text='Coordenadas de Translacao:')
+    msg.pack()
+
+    xlabel = Tkinter.Label(top, text='X:')
+    xentry = Tkinter.Entry(top)
+    xentry.insert(0, '0')
+    ylabel = Tkinter.Label(top, text='Y:')
+    yentry = Tkinter.Entry(top)
+    yentry.insert(0, '0')
+
+    xlabel.pack()
+    xentry.pack()
+    ylabel.pack()
+    yentry.pack()
+
+    button = Tkinter.Button(
+        top, text="Transladar", command=lambda: press(
+            xentry.get(), yentry.get()))
+    button.pack()
+
+
+def _scale(img, canvas):
+    def press(xtext, ytext):
+        top.destroy()
+        transformation.scale(img, canvas, float(xtext), float(ytext))
+
+    top = Tkinter.Toplevel(width=300, height=300)
+    top.title('Mudanca de Escala')
+
+    msg = Tkinter.Message(top, text='Taxas de Transformacao:')
+    msg.pack()
+
+    xlabel = Tkinter.Label(top, text='X:')
+    xentry = Tkinter.Entry(top)
+    xentry.insert(0, '1.0')
+    ylabel = Tkinter.Label(top, text='Y:')
+    yentry = Tkinter.Entry(top)
+    yentry.insert(0, '1.0')
+
+    xlabel.pack()
+    xentry.pack()
+    ylabel.pack()
+    yentry.pack()
+
+    button = Tkinter.Button(
+        top, text="Mudar Escala", command=lambda: press(
+            xentry.get(), yentry.get()))
+    button.pack()
+
+
+def _rotate(img, canvas):
+    def press(atext):
+        top.destroy()
+        transformation.rotate(img, canvas, float(atext))
+
+    top = Tkinter.Toplevel(width=300, height=300)
+    top.title('Rotacao')
+
+    msg = Tkinter.Message(top, text=('Angulo em graus (angulos negativos '
+                                     'para giro anti-horario):'))
+    msg.pack()
+
+    alabel = Tkinter.Label(top, text='X:')
+    aentry = Tkinter.Entry(top)
+    aentry.insert(0, '0.0')
+
+    alabel.pack()
+    aentry.pack()
+
+    button = Tkinter.Button(
+        top, text="Rotacionar", command=lambda: press(
+            aentry.get()))
+    button.pack()
 
 
 def main():
     root = Tkinter.Tk()
+    root.title('Trabalho de CG')
     eventmng = WaitingEv()
 
     canvas = Tkinter.Canvas(width=CANVASWIDTH, height=CANVASHEIGHT)
     eventmng.canvas = canvas
     image = Image.new('1', (CANVASWIDTH, CANVASHEIGHT), '#ffffff')
     eventmng.img = image
-    clearbutton = Tkinter.Button(root, text="limpar",
+    drawpane = Tkinter.Frame(root)
+    transpane = Tkinter.Frame(root)
+    clearbutton = Tkinter.Button(drawpane, text="limpar",
                                  command=lambda: _clear(image, canvas))
-    linebutton = Tkinter.Button(root, text="linha",
+    linebutton = Tkinter.Button(drawpane, text="linha",
                                 command=lambda: eventmng.set_waitingf(
                                     2, _drawline))
-    rectbutton = Tkinter.Button(root, text="retangulo",
+    rectbutton = Tkinter.Button(drawpane, text="retangulo",
                                 command=lambda: eventmng.set_waitingf(
                                     2, _drawrect))
-    trianbutton = Tkinter.Button(root, text="triangulo",
+    zoombutton = Tkinter.Button(transpane, text="zoom",
+                                command=lambda: eventmng.set_waitingf(
+                                    2, _zoom))
+    trianbutton = Tkinter.Button(drawpane, text="triangulo",
                                  command=lambda: eventmng.set_waitingf(
                                      3, _drawtrian))
-    circlebutton = Tkinter.Button(root, text="circulo",
+    circlebutton = Tkinter.Button(drawpane, text="circulo",
                                   command=lambda: eventmng.set_waitingf(
                                       2, _drawcircle))
-    # rotatebutton = Tkinter.Button(root, text="rotacao", command=drawline)
-    # scalebutton = Tkinter.Button(root, text="espelhar", command=drawline)
-    movebutton = Tkinter.Button(root, text="translacao",
+    rotatebutton = Tkinter.Button(transpane, text="rotacao",
+                                  command=lambda: _rotate(image, canvas))
+    scalebutton = Tkinter.Button(transpane, text="escala",
+                                 command=lambda: _scale(image, canvas))
+    movebutton = Tkinter.Button(transpane, text="translacao",
                                 command=lambda: _move(image, canvas))
-    canvas.pack()
-    clearbutton.pack()
-    linebutton.pack()
-    rectbutton.pack()
-    trianbutton.pack()
-    circlebutton.pack()
-    # rotatebutton.pack()
-    # scalebutton.pack()
-    movebutton.pack()
+    canvas.pack(side=Tkinter.TOP)
+    transpane.pack(side=Tkinter.BOTTOM)
+    drawpane.pack(side=Tkinter.BOTTOM)
+    clearbutton.pack(side=Tkinter.RIGHT)
+    linebutton.pack(side=Tkinter.RIGHT)
+    rectbutton.pack(side=Tkinter.RIGHT)
+    zoombutton.pack(side=Tkinter.RIGHT)
+    trianbutton.pack(side=Tkinter.RIGHT)
+    circlebutton.pack(side=Tkinter.RIGHT)
+    rotatebutton.pack(side=Tkinter.RIGHT)
+    scalebutton.pack(side=Tkinter.RIGHT)
+    movebutton.pack(side=Tkinter.RIGHT)
 
     canvas._image_tk = ImageTk.PhotoImage(image)
     canvas._image_id = canvas.create_image(0, 0, image=canvas._image_tk, anchor='nw')
